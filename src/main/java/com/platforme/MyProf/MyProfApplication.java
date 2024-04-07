b72e29664f28c761c2069417cadc88411ac30b89
MFRADJ <mohamed-fradj@outlook.com>
2024-04-07 03:35:26 +0200

    Initial commit

---
+++ src/main/java/com/platforme/MyProf/MyProfApplication.java
@@ -0,0 +1,13 @@
+package com.platforme.MyProf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyProfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyProfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/platforme/MyProf/MyProfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.platforme.MyProf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyProfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
